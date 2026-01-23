c6a3240d4f60e98afb975ca090b38b5e3cd275ce
Leonard <nardpueblos@gmail.com>
2026-01-23 13:29:03 +0800

    added featured blog and some styling

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 
+// Fetching from JSON File
+async function fetchJSON(filepath) {
+    return fetch(filepath)
+        .then(res => res.json())
+        .then(data => data)
+}
+
+
+function renderBlogPreview(data) {
+    
+}
 
 
 // Listening to Developer Mode
 document.getElementById('developer').addEventListener('click', (e) => {
     if (e.detail === 3) {
-
+        document.getElementById('add-blog').style.display = 'block'
     }
 })
