a6c01683358615054a6986bd8381d6efa072a801
Leonard <nardpueblos@gmail.com>
2026-01-23 16:24:01 +0800

    added blog cards

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 
 // Fetching from JSON File
-async function fetchJSON(filepath) {
-    return fetch(filepath)
-        .then(res => res.json())
-        .then(data => data)
+async function fetchJSON() {
+    const res = await fetch('data.json');
+    const data = await res.json();
+    return data;
 }
 
-
 function renderBlogPreview(data) {
-    
+    const blogContainer = document.getElementById('blogs-container')
+
+    data.forEach(blog => {
+        const blogDiv = document.createElement('div')
+        blogDiv.classList.add('blogs')
+        blogDiv.innerHTML = `
+            <img src="images/${blog.image}" alt="" class="blog-img">
+            <span class="blog-date">${blog.date}</span>
+            <h2 class="blog-title">${blog.title}</h2>
+            <p class="blog-shortdesc">${blog.short_desc}</p>
+        `
+        blogContainer.appendChild(blogDiv)
+    });
 }
 
 
@@ -18,3 +29,11 @@ document.getElementById('developer').addEventListener('click', (e) => {
         document.getElementById('add-blog').style.display = 'block'
     }
 })
+
+
+async function init() {
+    const data = await fetchJSON();
+    renderBlogPreview(data);
+}
+
+init();
